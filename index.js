17c371a5484002e72d62cde4ac7dc32ebfd8f593
CannsCoding <143218829+CannsCoding@users.noreply.github.com>
2025-10-01 00:21:49 +0000

    1 commit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 (function(window, _) {
-  window.myLibraryName = window.myLibraryName || {
-    numz: {},
+  window.gamelibTemp = window.gamelibTemp || {
+    numz: {
+        calculateDistance(obj1, obj2) {
+            return ((obj1.x - obj2.x) ** 2 + (obj1.y - obj2.y) ** 2) * (1/2)
+        }
+    },
     phyz: {},
   };
 }(window, window._));
