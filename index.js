0bdca7dcf202c172e6b8f2287081ecae50e48239
CannsCoding <143218829+CannsCoding@users.noreply.github.com>
2025-10-07 22:36:24 +0000

    “done”

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function(window, _) {
-  window.gamelibTemp = window.gamelibTemp || {
+  window.gamelibTemp = window.gameLibTemp || {
     numz: {
         calculateDistance(obj1, obj2) {
             return ((obj1.x - obj2.x) ** 2 + (obj1.y - obj2.y) ** 2) * (1/2)
